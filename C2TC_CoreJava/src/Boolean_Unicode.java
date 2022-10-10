bddb4f01a9fd392e04cb3b0856a2f286bc3cf5d1
Shashank <Shashank@DESKTOP-OJDI5OH>
2022-10-10 19:37:27 +0530

    second commit

---
+++ C2TC_CoreJava/src/Boolean_Unicode.java
@@ -0,0 +1,30 @@
+
+public class Boolean_Unicode {
+
+	public static void main(String[] args) {
+//		
+//		int x = 10, y=20;
+//
+//		System.out.println(x+y);
+//
+//		System.out.println(-x);
+//
+//		System.out.println(++y);
+		
+		int x = 4;
+
+		x += 5;  x=x+5;
+
+		//x-5; // x = x-5
+
+		//x = 5; // x = x*5
+
+		//x /= 4; // x = x/4
+
+		//X%= 5; // x = x % 5
+
+		System.out.println(x);	
+
+	}
+
+}
---
+++ C2TC_CoreJava/src/TyprCastingDemo.java
@@ -0,0 +1,11 @@
+
+public class TyprCastingDemo {
+
+	public static void main(String[] args) {
+
+		float f = 10.532f;
+		long l = (long) f;
+		System.out.println(l);
+	}
+
+}
